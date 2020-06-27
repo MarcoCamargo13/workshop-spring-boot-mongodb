4190066dd41457643cd097e7e3415569d45e4be8
marco <marcopcamargo@gmail.com>
2020-06-27 18:55:35 -0300

    Operação de instanciação da base de dados

---
+++ src/main/java/com/educandoweb/workshopmongo/config/Instantiation.java
@@ -0,0 +1,36 @@
+package com.educandoweb.workshopmongo.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import com.educandoweb.workshopmongo.domain.User;
+import com.educandoweb.workshopmongo.repository.UserRepository;
+
+@Configuration // para indicar que esta interface é uma classe de configuração
+public class Instantiation implements CommandLineRunner {
+
+	@Autowired
+	private UserRepository userRepository;
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		userRepository.deleteAll(); // para deletar todos os registros de ususario do bD
+
+		// usuario para quando inicalizarmos a plicação os usuarios serão carregados no
+		// BD
+		User maria = new User(null, "Maria Brown", "maria@gmail.com");
+		User alex = new User(null, "Alex Green", "alex@gmail.com");
+		User bob = new User(null, "Bob Grey", "bob@gmail.com");
+
+		// userRepository.save(Arrays.asList(maria, alex, bob)));
+
+		userRepository.save(maria);
+		userRepository.save(alex);
+		userRepository.save(bob);
+	}
+
+}
