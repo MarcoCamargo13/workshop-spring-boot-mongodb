b533c87d1f14ec98111891c5155a6cfbd6bfca8d
marco <marcopcamargo@gmail.com>
2020-07-01 16:58:16 -0300

    Referenciando os posts do usuário

--- src/main/java/com/educandoweb/workshopmongo/config/Instantiation.java
+++ src/main/java/com/educandoweb/workshopmongo/config/Instantiation.java
@@ -9,6 +9,8 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
 import com.educandoweb.workshopmongo.domain.Post;
+import com.educandoweb.workshopmongo.domain.User;
+import com.educandoweb.workshopmongo.dto.AuthorDTO;
 import com.educandoweb.workshopmongo.repository.PostRepository;
 import com.educandoweb.workshopmongo.repository.UserRepository;
 
@@ -27,23 +29,28 @@ public class Instantiation implements CommandLineRunner {
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:ss");
 		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
 
-		//userRepository.deleteAll(); // para deletar todos os registros de ususario do bD
+		userRepository.deleteAll(); // para deletar todos os registros de ususario do bD
 		postRepository.deleteAll(); // para deletar todos os registros de ususario do bD
 		
 		// usuario para quando inicalizarmos a plicação os usuarios serão carregados no
 		// BD
-		//User maria = new User(null, "Maria Brown", "maria@gmail.com");
-		//User alex = new User(null, "Alex Green", "alex@gmail.com");
-		//User bob = new User(null, "Bob Grey", "bob@gmail.com");
+		User maria = new User(null, "Maria Brown", "maria@gmail.com");
+		User alex = new User(null, "Alex Green", "alex@gmail.com");
+		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
-		Post post1 = new Post(null, sdf.parse("21/03/2018 13:00"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", "Maria Brown");
-		Post post2 = new Post(null, sdf.parse("19/08/2017 03:00"), "Bom dia!!", "Acordei ", "Maria Brown");
-		//userRepository.save(Arrays.asList(maria, alex, bob));
-
 		//userRepository.save(maria);
 		//userRepository.save(alex);
 		//userRepository.save(bob);
+		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+		
+		
+		
+		Post post1 = new Post(null, sdf.parse("21/03/2018 13:00"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("19/08/2017 03:00"), "Bom dia!!", "Acordei ", new AuthorDTO(alex));
+		
+
 		postRepository.saveAll((Iterable<Post>) Arrays.asList(post1, post2));
+		
 		
 		//postRepository.save(post1);
 		//postRepository.save(post2);
